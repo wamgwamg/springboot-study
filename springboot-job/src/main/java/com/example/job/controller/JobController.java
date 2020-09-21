6740e6bdbdb2c21e9428892e0e101c3512c43f66
wangye <915522192@qq.com>
2020-09-21 17:42:04 +0800

    springboot-job模块开发部分

---
+++ springboot-job/src/main/java/com/example/job/controller/JobController.java
@@ -0,0 +1,12 @@
+package com.example.job.controller;
+
+/**
+ * @author : wangye
+ * @date: 2020-09-18
+ * @description: 定时任务
+ */
+public class JobController {
+
+
+
+}
---
+++ springboot-job/src/main/java/com/example/job/entity/SysTask.java
@@ -0,0 +1,127 @@
+package com.example.job.entity;
+
+import java.util.Date;
+
+/**
+ * @author : wangye
+ * @date: 2020-09-18
+ * @description:
+ */
+public class SysTask {
+    private Long id;
+    private String jobName;
+    private String description;
+    private String cronExpression;
+    private String beanClass;
+    private String jobStatus;
+    private String jobGroup;
+    private String createUser;
+    private Date createTime;
+    private String updateUser;
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+    public String getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(String beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public String getJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(String jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    public String getJobGroup() {
+        return jobGroup;
+    }
+
+    public void setJobGroup(String jobGroup) {
+        this.jobGroup = jobGroup;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "SysTask{" +
+                "id=" + id +
+                ", jobName='" + jobName + '\'' +
+                ", description='" + description + '\'' +
+                ", cronExpression='" + cronExpression + '\'' +
+                ", beanClass='" + beanClass + '\'' +
+                ", jobStatus='" + jobStatus + '\'' +
+                ", jobGroup='" + jobGroup + '\'' +
+                ", createUser='" + createUser + '\'' +
+                ", createTime=" + createTime +
+                ", updateUser='" + updateUser + '\'' +
+                ", updateTime=" + updateTime +
+                '}';
+    }
+}
