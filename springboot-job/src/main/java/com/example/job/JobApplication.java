0725bc765b2c0edfc49244945bb8a45a6805374f
wangye <915522192@qq.com>
2020-10-01 11:11:18 +0800

    springboot修改

--- springboot-job/src/main/java/com/example/job/JobApplication.java
+++ springboot-job/src/main/java/com/example/job/JobApplication.java
@@ -2,10 +2,10 @@ package com.example.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class JobApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JobApplication.class, args);
     }
--- springboot-mail/src/main/java/com/example/mail/MailApplication.java
+++ springboot-mail/src/main/java/com/example/mail/MailApplication.java
@@ -2,8 +2,14 @@ package com.example.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+* description: 数据库信息也可以不配置，但是你需要声明一下 DataSourceAutoConfiguration
+* @return
+* @param
+*/
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class MailApplication {
 
     public static void main(String[] args) {
