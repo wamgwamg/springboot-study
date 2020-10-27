6e8ff42d916e8de0a2ff91d51e52588b3f7b5a0c
wangye <915522192@qq.com>
2020-10-27 20:37:29 +0800

    ThreadLocal全面解析实例

---
+++ skill/src/main/java/threadLocal/MyDemo.java
@@ -0,0 +1,34 @@
+package threadLocal;
+
+/**
+ * @author : wangye
+ * @date: 2020-10-27
+ * @description:
+ */
+public class MyDemo {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public static void main(String[] args) {
+        MyDemo demo = new MyDemo();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    demo.setContent(Thread.currentThread().getName() + "的数据");
+                    System.out.println("-----------------------");
+                    System.out.println(Thread.currentThread().getName() + "--->" + demo.getContent());
+                }
+            });
+            thread.setName("线程" + i);
+            thread.start();
+        }
+    }
+}
