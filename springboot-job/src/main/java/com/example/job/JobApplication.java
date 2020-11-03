74961082546e77e29d03468426c5a4c3d1dca11a
wangye <915522192@qq.com>
2020-11-03 16:51:31 +0800

    Druid 多数据源配置

--- springboot-job/src/main/java/com/example/job/JobApplication.java
+++ springboot-job/src/main/java/com/example/job/JobApplication.java
@@ -1,10 +1,20 @@
 package com.example.job;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication()
+
+@MapperScan("com.example.job.dao")
+/**去掉 DruidDataSourceAutoConfigure在DynamciDataSourceAutoConfiguration之前，其会注入一个DataSourceWrapper，
+会在原生的spring.datasource下找url,username,password等。而我们动态数据源的配置路径是变化的。
+ 在启动类加 @SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+ 或者在yml 添加
+
+
+ */
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class JobApplication {
     public static void main(String[] args) {
         SpringApplication.run(JobApplication.class, args);
--- springboot-job/src/main/java/com/example/job/controller/JobController.java
+++ springboot-job/src/main/java/com/example/job/controller/JobController.java
@@ -1,12 +1,29 @@
 package com.example.job.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.job.entity.SysTask;
+import com.example.job.service.SysTaskServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : wangye
  * @date: 2020-09-18
  * @description: 定时任务
  */
+@RestController
+@RequestMapping("/job")
 public class JobController {
 
+    @Autowired
+    public SysTaskServiceImpl sysTaskService;
 
 
+    @RequestMapping(method = RequestMethod.GET, path = "/getList")
+    public SysTask getList(String id) {
+
+        return sysTaskService.getOne(new QueryWrapper<SysTask>().eq("id", id));
+    }
 }
