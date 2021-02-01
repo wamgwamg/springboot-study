c49519838b3d2f4072dc864daeb8deed62983dfe
wangye <915522192@qq.com>
2021-02-01 20:11:22 +0800

    git-modify-测试

--- springboot-job/src/main/java/com/example/job/config/WebInterConfig.java
+++ springboot-job/src/main/java/com/example/job/config/WebInterConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,7 +30,7 @@ public class WebInterConfig implements WebMvcConfigurer {
     }
 
     /**
-     * 通过过滤器实现CORS拦截，对拦截器也是有效
+     * 第一种方法 通过过滤器实现CORS拦截，对拦截器也是有效
      */
     @Bean
     public CorsFilter corsFilter() {
@@ -69,8 +70,36 @@ public class WebInterConfig implements WebMvcConfigurer {
     }
 
 
-
-
-
-
+    /**
+     * 第二种方法
+     *详解响应头：
+     *
+     * Access-Control-Allow-Origin 该字段必填。它的值要么是请求时Origin字段的具体值，要么是一个*，表示接受任意域名的请求。
+     *
+     * Access-Control-Allow-Methods 该字段必填。它的值是逗号分隔的一个具体的字符串或者*，表明服务器支持的所有跨域请求的方法。
+     * 注意，返回的是所有支持的方法，而不单是浏览器请求的那个方法。这是为了避免多次"预检"请求。
+     *
+     * Access-Control-Expose-Headers 该字段可选。CORS请求时，XMLHttpRequest对象的getResponseHeader()方法只能拿到6个基本字段：
+     * Cache-Control、Content-Language、Content-Type、Expires、Last-Modified、Pragma。如果想拿到其他字段，
+     * 就必须在Access-Control-Expose-Headers里面指定。
+     *
+     * Access-Control-Allow-Credentials 该字段可选。它的值是一个布尔值，表示是否允许发送Cookie.默认情况下，
+     * 不发生Cookie，即：false。对服务器有特殊要求的请求，比如请求方法是PUT或DELETE，或者Content-Type字段的类型是application/json，
+     * 这个值只能设为true。如果服务器不要浏览器发送Cookie，删除该字段即可。
+     *
+     * Access-Control-Max-Age 该字段可选，用来指定本次预检请求的有效期，单位为秒。在有效期间，不用发出另一条预检请求。
+     *
+     * 顺便提一下，如果在开发中，发现每次发起请求都是两条，一次OPTIONS，一次正常请求，注意是每次，那么就需要配置Access-Control-Max-Age，避免每次都发出预检请求。
+     *
+     *
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
 }
